22796a50c30c471e2bd1122b6fb2c7b80f22b103
Crowdbotics <team@crowdbotics.com>
2023-11-06 06:20:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gc-railing-44359",
+  url: "https://gc-railing-44359.botics.co",
+  api: "https://gc-railing-44359.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
